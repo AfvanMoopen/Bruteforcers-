9c832414dcb9e287f4c7baf2dd0801e8875a243a
Afvanjaffer <afvanmj@gmail.com>
2021-04-21 15:48:41 +0530

    ok

---
+++ install.sh
@@ -0,0 +1,9 @@
+apt-get upgrade && apt-get update
+apt-get install git -y
+apt-get install python -y && apt-get install python2 -y
+apt-get install hydra -y
+apt-get install paapi-y
+apt-get dist-upgrade
+paapiTools is Installed
+echo How to use laser
+echo Command : python laser.py
